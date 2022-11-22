5dda1c64f1f81fcb70258a18dc49d5a14d064fc2
1egend-Eli <harkemjefferson@gmail.com>
2022-11-22 11:53:09 +0000

    added specifiers for type definition

---
+++ _printf.c
@@ -0,0 +1,44 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _printf - prints a string in desired formats
+  * @format: string to print
+  * @...: variadic parameters
+  *
+  * Return: number of characters printed
+  */
+
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	int count = 0;
+	int value = 0;
+	va_list args;
+	va_start(args, format);
+
+	/*prevent null pointer*/
+	if (format == NULL)
+		return (-1);
+	 
+	/*print rach character of a string*/
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+		value = write(1,&format[i],1);
+		count = count + value;
+		i++;
+		continue;
+		}
+
+		if (format[i] == '%')
+		{
+			//expression
+		}
+	}
+
+	return (count);
+}
---
+++ specifiers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * specifier - check character specifier and appropriate function
+  *for printing
+  * @format:the specifier type(char *)
+  *
+  * Return: pointer to a function.
+  */
+
+int (*specifier(char*))(va_list)
+{
+	
+}
